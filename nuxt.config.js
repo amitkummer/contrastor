63db4b0bf6a6a515081125ea4e4a3e2de888775c
Amit <amit.kummer@gmail.com>
2020-09-26 17:41:02 +0300

    Adjust vuetify default theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,16 +45,14 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+      theme: {
+        themes: {
+          light: {
+            primary: '#0f4c81',
+            accent: '#fe7d00',
+            info: '#1569b2',
+            error: colors.deepOrange.darken1,
+          },
         },
       },
     },
