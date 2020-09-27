5f889e62178f5194e140954c4bbadf1248b2591a
Amit <amit.kummer@gmail.com>
2020-09-27 18:55:52 +0300

    Add about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,29 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      <img src="v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <v-card outlined width="900px" class="mx-auto">
+    <v-card-title> About this app </v-card-title>
+    <v-card-text>
+      <p>
+        Contrast ratio of background and foreground colors in web development is
+        absolute critical to user experience and accessibility.
+      </p>
+      <p>
+        Because of that, I decided making this contrast calculator could be a
+        cool idea for making a Vue app and honing my skills with Nuxt.js.
+      </p>
+      <p>
+        The site is powered by
+        <v-chip link small href="https://vuejs.org/"> Vue </v-chip>
+        and built using
+        <v-chip link small href="https://nuxtjs.org/"> Nuxt </v-chip> and
+        <v-chip link small href="https://vuetifyjs.com/en/"> Vuetify </v-chip>.
+      </p>
+      <p>
+        The boilerplate for this app was generated using the wonderful
+        <v-chip link small href="https://github.com/nuxt/create-nuxt-app">
+          create-nuxt-app
+        </v-chip>
+        project.
+      </p>
+    </v-card-text>
+  </v-card>
 </template>
