18b5f8c3461cfddaaa498056d457be9b9d7d1fe4
amitkummer <amit.kummer@gmail.com>
2020-10-09 12:56:22 +0300

    Reside about card in a container to add margins

--- pages/about.vue
+++ pages/about.vue
@@ -1,29 +1,32 @@
 <template>
-  <v-card outlined width="900px" class="mx-auto">
-    <v-card-title> About this app </v-card-title>
-    <v-card-text>
-      <p>
-        Contrast ratio of background and foreground colors in web development is
-        absolute critical to user experience and accessibility.
-      </p>
-      <p>
-        Because of that, I decided making this contrast calculator could be a
-        cool idea for making a Vue app and honing my skills with Nuxt.js.
-      </p>
-      <p>
-        The site is powered by
-        <v-chip link small href="https://vuejs.org/"> Vue </v-chip>
-        and built using
-        <v-chip link small href="https://nuxtjs.org/"> Nuxt </v-chip> and
-        <v-chip link small href="https://vuetifyjs.com/en/"> Vuetify </v-chip>.
-      </p>
-      <p>
-        The boilerplate for this app was generated using the wonderful
-        <v-chip link small href="https://github.com/nuxt/create-nuxt-app">
-          create-nuxt-app
-        </v-chip>
-        project.
-      </p>
-    </v-card-text>
-  </v-card>
+  <v-container class="d-flex justify-center">
+    <v-card outlined width="900px" class="mx-auto">
+      <v-card-title> About this app </v-card-title>
+      <v-card-text>
+        <p>
+          Contrast ratio of background and foreground colors in web development
+          is absolute critical to user experience and accessibility.
+        </p>
+        <p>
+          Because of that, I decided making this contrast calculator could be a
+          cool idea for making a Vue app and honing my skills with Nuxt.js.
+        </p>
+        <p>
+          The site is powered by
+          <v-chip link small href="https://vuejs.org/"> Vue </v-chip>
+          and built using
+          <v-chip link small href="https://nuxtjs.org/"> Nuxt </v-chip> and
+          <v-chip link small href="https://vuetifyjs.com/en/"> Vuetify </v-chip
+          >.
+        </p>
+        <p>
+          The boilerplate for this app was generated using the wonderful
+          <v-chip link small href="https://github.com/nuxt/create-nuxt-app">
+            create-nuxt-app
+          </v-chip>
+          project.
+        </p>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
